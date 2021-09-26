161d8569c93774f4f210cb70b3a0c67519bc420c
Heitor Brunini <68927093+heitorbrunini@users.noreply.github.com>
2021-09-26 02:09:24 -0300

    Criando o controlador da MainView

---
+++ src/gui/MainViewController.java
@@ -0,0 +1,37 @@
+package gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.MenuItem;
+
+public class MainViewController implements Initializable {
+	@FXML
+	private MenuItem menuItemSobre;
+	@FXML
+	private MenuItem menuItemDepartamento;
+	@FXML
+	private MenuItem menuItemFuncionario;
+	
+	@FXML
+	public void onMenuItemSobreAction() {
+		
+	}
+	@FXML
+	public void onMenuItemDepartamentoAction() {
+		
+	}
+	@FXML
+	public void onMenuItemFuncionarioAction() {
+		
+	}
+	
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
