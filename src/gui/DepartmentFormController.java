4c177fccc94742efb0046e692de153c1f1499018
Heitor Brunini <68927093+heitorbrunini@users.noreply.github.com>
2021-10-21 04:50:05 -0300

    Mudando de Combobox para Radiobutton
    como são poucas opções de operações achei melhor colocar apenas RadioButton

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -7,22 +7,21 @@ import java.util.ResourceBundle;
 
 import gui.listeners.DataChangeListener;
 import gui.util.Alerts;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import model.entities.Department;
 import model.services.DepartmentService;
 
 public class DepartmentFormController implements Initializable {
 	private DepartmentService dept_service = new DepartmentService();
-	private List<DataChangeListener> listeners= new ArrayList<>();
+	private List<DataChangeListener> listeners = new ArrayList<>();
+	private Boolean New=false;
 	@FXML
 	private TextField textId;
 	@FXML
@@ -33,76 +32,76 @@ public class DepartmentFormController implements Initializable {
 	private Button save;
 	@FXML
 	private Button cancel;
+
 	@FXML
-	private ComboBox<String> operation;
+	private RadioButton radio1;
 	@FXML
-	private ObservableList<String> auxlist;
-	
-	//ação de salvar no banco
+	private RadioButton radio2;
+
+	// ação de salvar no banco
 	@FXML
 	public void onBtsaveAction(ActionEvent event) {
-		String Status = operation.getSelectionModel().getSelectedItem();
+
 		try {
 			if (dept_service != null) {
-				if (Status == "Atualizar") {
-					dept_service.update(new Department(Integer.parseInt(textId.getText()),textNome.getText()));
+				if (New==false) {
+					//dept_service.update(new Department(Integer.parseInt(textId.getText()), textNome.getText()));
 					notifyDataChangeListeners();
 					Alerts.currentStage(event).close();
-					Alerts.showAlert("Confirmação", "Atualização", "Você Atualizou um departamento!",
-							AlertType.INFORMATION);
-				} else if (Status == "Criar") {
-					dept_service.create(new Department(textNome.getText()));
-					System.out.println(textNome.getText());
-					notifyDataChangeListeners();
-					Alerts.currentStage(event).close();
-					Alerts.showAlert("Confirmação", "Criação", "Você criou um novo departamento!",
-							AlertType.INFORMATION);
+					Alerts.showAlert("Confirmação", "Atualização", "Você Atualizou um departamento!",AlertType.INFORMATION);
+				} else if (New) {
+					if (textNome.getText() == null || textNome.getText().isEmpty()) {
+						Labelerror.setText(" preencha o campo!");
+					} else {
+						// dept_service.create(new Department(textNome.getText()));
+						notifyDataChangeListeners();
+						Alerts.currentStage(event).close();
+						Alerts.showAlert("Confirmação", "Criação", "Você criou um novo departamento!",
+								AlertType.INFORMATION);
+					}
 				}
-			}
-			else {
+			} else {
 				throw new IllegalStateException("falha ao tentar executar a operação");
 			}
 		} catch (IllegalStateException e) {
 			Alerts.showAlert("ERROR", "Falha", e.getMessage(), AlertType.ERROR);
 		}
 	};
-	
-	public void onComboboxAction() {
-		String Status = operation.getSelectionModel().getSelectedItem();
-		if (Status == "Atualizar") {
-			textId.setEditable(true);
-		} else if (Status == "Criar") {
-			textId.setText("");
-			textId.setEditable(false);
-		}
-	}
 
 	@FXML
 	public void onBtCancelAction(ActionEvent event) {
 		Alerts.currentStage(event).close();
-		Alerts.showAlert("Confirmação", "Cancelamento", "Você cancelou criar/alterar um departamento!",AlertType.INFORMATION);
+		Alerts.showAlert("Confirmação", "Cancelamento", "Você cancelou criar/alterar um departamento!",
+				AlertType.INFORMATION);
 	};
 
-	//Atualizar view
+	// Atualizar view
 	private void notifyDataChangeListeners() {
-		for (DataChangeListener listener: listeners) {
+		for (DataChangeListener listener : listeners) {
 			listener.onDataChanged();
 		}
 	}
-	public void subscripeDataChangeListener(DataChangeListener listener){
+
+	public void subscripeDataChangeListener(DataChangeListener listener) {
 		listeners.add(listener);
 	}
-	
+	//definir se o banco vai atualizar ou criar
+	public void onRadio1Action() {
+		textId.setEditable(false);
+		radio2.setSelected(false);		
+		this.New=true;
+	}
+
+	public void onRadio2Action() {
+		textId.setEditable(true);
+		radio1.setSelected(false);
+		this.New=false;
+	}
 
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-		List<String> list = new ArrayList<>();
-		list.add("Atualizar");
-		list.add("Criar");
-		auxlist = FXCollections.observableArrayList(list);
-		operation.setItems(auxlist);
+
 	}
-	
 
 }
